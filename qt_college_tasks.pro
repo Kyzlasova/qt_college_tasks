e5232df8d45706c9fa01dde5403651bfe8bf3cf0
m.kyzlasova <kyzlasovam@inbox.ru>
2021-05-29 12:50:10 +0300

    Initial commit (создала проект)

---
+++ qt_college_tasks.pro
@@ -0,0 +1,4 @@
+TEMPLATE = subdirs
+
+SUBDIRS += \
+    task1_singleton
---
+++ task1_singleton/main.cpp
@@ -0,0 +1,9 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    // НИЧЕГО НЕ ПОНЯТНО, ААААААА!!!!111
+
+    return a.exec();
+}
