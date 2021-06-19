9a3a639c3abeb72db9295a35080aaad8422ae6aa
m.kyzlasova <kyzlasovam@inbox.ru>
2021-06-19 12:53:55 +0300

    Черновик интерфейса

--- task3_filewatcher/main.cpp
+++ task3_filewatcher/main.cpp
@@ -3,33 +3,12 @@
 #include <QFileInfo>
 #include <QDir>
 
-#include "individualfilestrategy.h"
-#include "overallfilestrategy.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    IndividualFileStrategy strategy1;
-    OverallFolderStrategy strategy2;
-
-    QDir dir(QDir::currentPath());
-    dir.cdUp();
-
-    strategy1.Calculate(dir.absolutePath());
-    strategy2.Calculate(dir.absolutePath());
-
-    auto res1 = strategy1.GetStatistics();
-    qInfo() << "Per Item Strategy: \n";
-    for (const auto& item : res1.keys()) {
-        qInfo() << QFileInfo(item).filePath() << " : " << res1[item];
-    }
-
-    auto res2 = strategy2.GetStatistics();
-    qInfo() << "\nOverall Strategy: \n";
-    for (const auto& item : res2.keys()) {
-        qInfo() << item << " : " << res2[item];
-    }
-
+    QApplication a(argc, argv);
+    MainWindow wnd;
+    wnd.show();
     return a.exec();
 }
--- task3_filewatcher/task3_filewatcher.pro
+++ task3_filewatcher/task3_filewatcher.pro
@@ -5,12 +5,17 @@ CONFIG -= app_bundle
 
 SOURCES += \
     individualfilestrategy.cpp \
+    mainwindow.cpp \
     overallfilestrategy.cpp \
     main.cpp
 
 HEADERS += \
     abstractstrategy.h \
     individualfilestrategy.h \
+    mainwindow.h \
     overallfilestrategy.h
 
 DESTDIR = $$PWD/../out
+
+FORMS += \
+    mainwindow.ui
